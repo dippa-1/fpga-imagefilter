57834c61327403cfdb6c93aa9b025a802fb356ec
Dominik Helfenstein <46935291+dippa-1@users.noreply.github.com>
2022-02-26 18:50:26 +0100

    Update generate_filter_test_values.py

--- generate_filter_test_values.py
+++ generate_filter_test_values.py
@@ -5,7 +5,6 @@ image = [255, 255, 255, 0, 0, 0, 0, 0, 0]
 SobelY = [1, 2, 1, 0, 0, 0, -1, -2, -1]
 SobelX = [-1, 0, 1, -2, 0, 2, -1, 0, 1]
 Sobel = np.array([0, 1, 1, -1, 0, 1, -1, -1, 0])/6
-# Sobel = np.add(SobelX, SobelY)/12
 print('Sobel', Sobel)
 
 outputs = []
@@ -20,7 +19,5 @@ for try_num in range(16):
     print(f'input => (x"{image[1]:02x}", x"{image[2]:02x}", x"{image[3]:02x}", x"{image[5]:02x}", x"{image[6]:02x}", x"{image[7]:02x}"),')
     print(f'output => x"{output:02x}"')
     print('),')
-    # print('in ', image)
-    # print('out', int(output) + 128)
 
 print(f"Min: {np.min(outputs)}, Max: {np.max(outputs)}")
